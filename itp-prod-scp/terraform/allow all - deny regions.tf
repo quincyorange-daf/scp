198bc27f3f23dd082668707c7cd1520f64273c9b
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2023-10-17 15:24:40 +1000

    initial commit

---
+++ itp-prod-scp/terraform/allow all - deny regions.tf
@@ -0,0 +1,55 @@
+# ---------------------------------------- # 
+# Service Control Policies for all accounts
+# ---------------------------------------- #
+
+# ---------------------------- #
+# ALLOW ALL
+# ---------------------------- #
+
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*"
+    }
+  ]
+},
+
+# ---------------------------------------- # 
+# Service Control Policies for all accounts
+# ---------------------------------------- #
+
+# ---------------------------- #
+# REGION RESTRICTION 
+# ---------------------------- #
+
+data "aws_iam_policy_document" "restrict_regions" {
+  statement {
+    sid       = "RegionRestriction"
+    effect    = "Deny"
+    actions   = ["*"]
+    resources = ["*"]
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:RequestedRegion"
+
+      values = [
+        "us-east-1"
+      ]
+    }
+  }
+}
+
+resource "aws_organizations_policy" "restrict_regions" {
+  name        = "restrict_regions"
+  description = "Deny all regions except US East 1."
+  content     = data.aws_iam_policy_document.restrict_regions.json
+}
+
+resource "aws_organizations_policy_attachment" "restrict_regions_on_root" {
+  policy_id = aws_organizations_policy.restrict_regions.id
+  target_id = aws_organizations_organization.this.roots[0].id
+}
