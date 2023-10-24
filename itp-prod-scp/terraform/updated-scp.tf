9b395cf7ec3a818bfd6343161717c761210aba3e
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2023-10-24 11:15:07 +1000

    Update updated-scp.tf

--- itp-prod-scp/terraform/updated-scp.tf
+++ itp-prod-scp/terraform/updated-scp.tf
@@ -8,6 +8,12 @@
 # REGION RESTRICTION 
 # ---------------------------- #
 
+data "aws_organizations_organization" "org" {}
+
+data "aws_organizations_organizational_units" "ou" {
+  parent_id = data.aws_organizations_organization.org.roots[0].id
+}
+
 data "aws_iam_policy_document" "restrict_regions" {
   statement {
     sid = "RegionRestriction"
