f4d6fe05f5cc994601119dcc23d25f7e1463d401
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2023-10-24 16:06:27 +1000

    Update AllowAll.tf

--- itp-prod-scp/terraform/AllowAll.tf
+++ itp-prod-scp/terraform/AllowAll.tf
@@ -1,22 +1,15 @@
+data "aws_iam_policy_document" "allow_all_access" {
 
-data "aws_iam_policy_document" "AllowAllResources" {
   statement {
-    sid    = "AllowAll"
-    effect = "Allow"
-    actions = ["*"]
+    sid       = "AllowAllAccess"
+    effect    = "Allow"
+    actions   = ["*"]
     resources = ["*"]
- 
-  condition {
-    test     = "StringEquals"
-    variable = "aws:RequestedRegion"
-
-    values   = ["ap-southeast-2", "ap-southeast-4"]
-    }
   }
 }
 
 resource "aws_organizations_policy" "AllowAll" {
   name        = "AllowAllResources"
   description = "Allow all resources in ap-southeast-2 and 4"
-  content     = data.aws_iam_policy_document.AllowAllResources.json
+  content     = data.aws_iam_policy_document.allow_all_access.json
 }
