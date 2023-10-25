29c6df6a6aee408626acc205c2e92c027a7e47bc
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2023-10-26 08:30:08 +1000

    Update TagPolicy.tf

--- itp-prod-scp/terraform/TagPolicy.tf
+++ itp-prod-scp/terraform/TagPolicy.tf
@@ -1,26 +1,24 @@
 resource "aws_organizations_policy" "TagsAppregid" {
-  name = "TagsAppregid"
-  type = "TAG_POLICY"
-  content = <<CONTENT
-{
-    "tags": {
-        "APPREGID": {
-            "tag_key": {
-                "@@assign": "APPREGID"
-            },
-            "tag_value": {
-                "@@assign": [
-                    "APPREG****",
-                    "APPREG0000"
-                ]
-            },
-            "enforced_for": {
-                "@@assign": [
-                    "EC2:*"
-                ]
-            }
+  name   = "TagsAppregid"
+  type   = "TAG_POLICY"
+  content = jsonencode({
+    tags = {
+      "APPREGID" = {
+        tag_key = {
+          "@@assign" = "APPREGID"
         }
+        tag_value = {
+          "@@assign" = [
+            "APPREG****",
+            "APPREG0000"
+          ]
+        }
+        enforced_for = {
+          "@@assign" = [
+            "EC2:*"
+          ]
+        }
+      }
     }
-}
-CONTENT
+  })
 }
