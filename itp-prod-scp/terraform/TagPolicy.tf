65590dbe0ad9c285039d0b463dec6f55e7adb8a1
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2023-10-26 15:57:57 +1000

    Update TagPolicy.tf

--- itp-prod-scp/terraform/TagPolicy.tf
+++ itp-prod-scp/terraform/TagPolicy.tf
@@ -1,24 +1,28 @@
-resource "aws_organizations_policy" "TagsAppregid" {
-  name   = "TagsAppregid"
-  type   = "TAG_POLICY"
-  content = jsonencode({
-    tags = {
-      "APPREGID" = {
-        tag_key = {
-          "@@assign" = "APPREGID"
-        }
-        tag_value = {
-          "@@assign" = [
-            "APPREG****",
-            "APPREG0000"
-          ]
-        }
-        enforced_for = {
-          "@@assign" = [
-            "EC2:*"
-          ]
-        }
-      }
+
+data "aws_iam_policy_document" "require_appreg_tags" {
+  statement {
+    sid    = "RequireAppregTag"
+    effect = "Deny"
+    actions = [
+      "ec2:RunInstances",
+      "ec2:CreateVolume"
+    ]
+    resources = [
+      "arn:aws:ec2:*:*:instance/*",
+      "arn:aws:ec2:*:*:volume/*"
+    ]
+
+    condition {
+      test     = "Null"
+      variable = "aws:RequestTag/APPREGID"
+
+      values = ["true"]
     }
-  })
+  }
+}
+
+resource "aws_organizations_policy" "require_appreg_tags" {
+  name        = "require_appreg_tags"
+  description = "APPREGID tag is required for EC2 instances and volumes."
+  content     = data.aws_iam_policy_document.require_ec2_tags.json
 }
