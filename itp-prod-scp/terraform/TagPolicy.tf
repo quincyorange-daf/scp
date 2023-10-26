736bf7a3d1773a205320b1877515f1d4ead1a4d5
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2023-10-26 16:00:15 +1000

    Update TagPolicy.tf

--- itp-prod-scp/terraform/TagPolicy.tf
+++ itp-prod-scp/terraform/TagPolicy.tf
@@ -24,5 +24,5 @@ data "aws_iam_policy_document" "require_appreg_tags" {
 resource "aws_organizations_policy" "require_appreg_tags" {
   name        = "require_appreg_tags"
   description = "APPREGID tag is required for EC2 instances and volumes."
-  content     = data.aws_iam_policy_document.require_ec2_tags.json
+  content     = data.aws_iam_policy_document.require_appreg_tags.json
 }
