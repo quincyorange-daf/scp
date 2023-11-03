006ac55774387381df1cd542502983e56bea8384
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2023-11-03 11:49:20 +1000

    update-s3 scp

--- itp-prod-scp/terraform/DenyLeaveOrg.tf
+++ itp-prod-scp/terraform/DenyLeaveOrg.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "PreventLeavingAWSOrganization" {
   statement {
     sid       = "PreventLeavingAWSOrganization"
     effect    = "Deny"
-    actions   = "organizations:LeaveOrganization"
+    actions   = ["organizations:LeaveOrganization"]
     resources = ["*"]
 
     condition {
@@ -23,7 +23,7 @@ data "aws_iam_policy_document" "PreventLeavingAWSOrganization" {
 #### create the policy and attach the policy rules ########
 resource "aws_organizations_policy" "deny_igw_org" {
   name        = "DenyIGWOrg"
-  description = "Allow certain EC2 instance types only."
+  description = "Deny the account from leaving the Organisation"
   content = [
     data.aws_iam_policy_document.PreventLeavingAWSOrganization.json
   ]
--- itp-prod-scp/terraform/DenyPublicS3.tf
+++ itp-prod-scp/terraform/DenyPublicS3.tf
@@ -1,28 +1,22 @@
 # ***** Deny access to leave the organization *****
-data "aws_iam_policy_document" "PreventLeaveOrganization" {
+data "aws_iam_policy_document" "PreventPublicS3Bucket" {
   statement {
-    sid    = "PreventLeaveOrganization"
+    sid    = "PreventPublicS3Bucket"
     effect = "Deny"
     actions = [
-      "organizations:LeaveOrganization"
+      "s3:PutBucketPublicAccessBlock",
+      "s3:PutAccountPublicAccessBlock"
     ]
     resources = ["*"]
-
-    condition {
-      test     = "StringNotLike"
-      variable = "aws:PrincipalARN"
-      values = [
-        "arn:aws:iam::*:role/aws-reserved/sso.amazonaws.com/ap-southeast-2/AWSReservedSSO_HybridCloud*"
-      ]
-    }
   }
 }
 
+
 # Create the policy and attach the policy rules
-resource "aws_organizations_policy" "deny_leave_organization" {
+resource "aws_organizations_policy" "PreventPublicS3Bucket" {
   name        = "DenyLeaveOrganization"
-  description = "Deny leaving the organization."
+  description = "Prevent public S3 bucket creation."
   content = [
-    data.aws_iam_policy_document.PreventLeaveOrganization.json
+    data.aws_iam_policy_document.PreventPublicS3Bucket.json
   ]
 }
