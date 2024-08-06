db9fd67279d75934c10ff7b55808376fc790c78a
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2024-08-06 15:47:34 +1000

    Update providers.tf

--- itp-prod-scp/providers.tf
+++ itp-prod-scp/providers.tf
@@ -1,12 +1,10 @@
 terraform {
+  required_version = ">= 1.7.0, < 2.0.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-}
-
-terraform {
-  required_version = "1.1.7"
 }
