cc3eab20fe5777b89c7f2d31f8f1e41aa34f6157
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2024-08-06 15:23:07 +1000

    Update TagPolicy.tf

--- itp-prod-scp/terraform/TagPolicy.tf
+++ itp-prod-scp/terraform/TagPolicy.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.7.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 data "aws_iam_policy_document" "require_appreg_tags" {
   statement {
     sid    = "RequireAppregTag"
